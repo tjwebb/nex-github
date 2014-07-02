df59ad16917e2676e471b022d5fb1f6988477311
Travis Webb <me@traviswebb.com>
2014-07-02 15:20:29 -0400

    added stub for private tests

--- spec.js
+++ spec.js
@@ -6,17 +6,40 @@ describe('nex-github', function () {
 
   describe('#getRelease', function () {
 
-    it('should download a public release without authentication', function (done) {
-      this.timeout(5000);
+    describe('@public', function () {
 
-      github.getRelease({ org: 'tjwebb', repo: 'nex' })
-        .then(function () {
-          assert.isTrue(true);
-          done();
-        }, function () {
-          assert.fail();
-        });
+      it('should download a public release without authentication', function (done) {
+        this.timeout(5000);
+
+        github.getRelease({ org: 'tjwebb', repo: 'nex' })
+          .then(function () {
+            assert.isTrue(true);
+            done();
+          }, function () {
+            assert.fail();
+          });
+      });
+
     });
+    describe('@private', function () {
+      /**
+       * This can be un-skipped for local development
+       */
+      it('should download a private release with authentication', function (done) {
+        this.timeout(5000);
 
+        github.getRelease({
+          org: 'xtuple',
+          repo: 'xtuple-server-commercial',
+          private: true
+        })
+          .then(function () {
+            assert.isTrue(true);
+            done();
+          }, function () {
+            assert.fail();
+          });
+      });
+    });
   });
 });
