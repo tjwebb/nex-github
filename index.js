d0b4b1316c64ceafcbb2d412f71adb59dc71afa7
Travis Webb <me@traviswebb.com>
2014-07-13 18:35:07 -0400

    use native es6 promises

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-/* jshint expr:true */
 var proc = require('child_process'),
   path = require('path'),
   fs = require('fs'),
   _ = require('lodash'),
-  Q = require('q'),
   congruence = require('congruence'),
   home = require('home-dir'),
   prompt = require('prompt');
@@ -60,51 +58,47 @@ function getTag (options) {
 var github = exports;
 
 github.storeCredentials = function () {
-  var deferred = Q.defer();
-
   log.info('authentication', 'This tool will create a .git-credentials file');
   log.info('authentication', 'https://www.kernel.org/pub/software/scm/git/docs/git-credential-store.html');
   log.info('authentication', 'Please enter your Github login info');
 
   prompt.start();
-  prompt.get([ 'username', 'password' ], function (err, result) {
-    if (err) {
-      console.log();
-      log.warn('prompt', 'canceled');
-      process.exit(1);
-    }
-    var gitCredentials = path.resolve(home(), '.git-credentials');
-    fs.writeFileSync(gitCredentials, 'https://'+ getGithubUser(options) + '@github.com');
-    fs.chmodSync(gitCredentials, '700');
 
-    log.info('authentication', '~/.git-credentials file created');
-    deferred.resolve();
+  return new Promise(function (resolve, reject) {
+    prompt.get([ 'username', 'password' ], function (err, result) {
+      if (err) {
+        console.log();
+        log.warn('prompt', 'canceled');
+        reject();
+      }
+      var gitCredentials = path.resolve(home(), '.git-credentials');
+      fs.writeFileSync(gitCredentials, 'https://'+ getGithubUser(options) + '@github.com');
+      fs.chmodSync(gitCredentials, '700');
+
+      log.info('authentication', '~/.git-credentials file created');
+      resolve();
+    });
   });
-
-  return deferred.promise;
 };
 
 github.showPrompt = function (options) {
-  var deferred = Q.defer();
-
   log.info('authentication', 'Authentication via Github is required.');
 
   prompt.start();
-  prompt.get(_.difference(_.keys(prompt.properties), _.keys(options)), function (err, result) {
-    if (err) {
-      console.log();
-      log.warn('prompt', 'canceled');
-      process.exit(1);
-    }
-    deferred.resolve(github.getRelease(_.extend({ }, result, options)));
-  });
 
-  return deferred.promise;
+  return new Promise(function (resolve, reject) {
+    prompt.get(_.difference(_.keys(prompt.properties), _.keys(options)), function (err, result) {
+      if (err) {
+        console.log();
+        log.warn('prompt', 'canceled');
+        process.exit(1);
+      }
+      resolve(github.getRelease(_.extend({ }, result, options)));
+    });
+  });
 };
 
 github.getRelease = function (options) {
-  var deferred = Q.defer();
-
   options = _.defaults(options || { }, {
     private: false,
     version: 'master'
@@ -127,32 +121,32 @@ github.getRelease = function (options) {
   log.verbose('tarball url', getCurlUrl(options));
   log.http('downloading', getModuleName(options));
 
-  var child = proc.exec(curl);
-  child.on('exit', function (err, code) {
-    if (err) deferred.reject(err);
+  return new Promise(function (resolve, reject) {
+    var child = proc.exec(curl);
+    child.on('exit', function (err, code) {
+      if (err) reject(err);
 
-    log.http('done', getFilename(options));
+      log.http('done', getFilename(options));
 
-    if (!fs.existsSync(getFilename(options))) {
-      log.verbose('downloaded slug', getFilename(options), 'does not exist');
-      deferred.reject(new Error('Release was not successfully downloaded.'));
-    }
-    else if (/Hello future GitHubber/.test(fs.readFileSync(getFilename(options)).toString())) {
-      log.verbose('authentication', 'failed');
-      deferred.reject(new Error('Incorrect Github credentials/info was provided'));
-    }
-    else {
-      log.verbose('curl exit', getFilename(options));
-      if (options.private) {
-        var gitCredentials = path.resolve(home(), '.git-credentials');
-        fs.writeFileSync(gitCredentials, 'https://'+ getGithubUser(options) + '@github.com');
-        fs.chmodSync(gitCredentials, '700');
+      if (!fs.existsSync(getFilename(options))) {
+        log.verbose('downloaded slug', getFilename(options), 'does not exist');
+        reject(new Error('Release was not successfully downloaded.'));
       }
-      deferred.resolve(path.resolve(getFilename(options)));
-    }
+      else if (/Hello future GitHubber/.test(fs.readFileSync(getFilename(options)).toString())) {
+        log.verbose('authentication', 'failed');
+        reject(new Error('Incorrect Github credentials/info was provided'));
+      }
+      else {
+        log.verbose('curl exit', getFilename(options));
+        if (options.private) {
+          var gitCredentials = path.resolve(home(), '.git-credentials');
+          fs.writeFileSync(gitCredentials, 'https://'+ getGithubUser(options) + '@github.com');
+          fs.chmodSync(gitCredentials, '700');
+        }
+        resolve(path.resolve(getFilename(options)));
+      }
+    });
   });
-
-  return deferred.promise;
 };
 
 if (require.main === module) github.getRelease();
